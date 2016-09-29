3ba87ae216158d10192f765167a2703f9d4999d5
Rakas <drakas@netsuite.com>
2016-09-29 15:08:08 -0400

    lolok

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
     public Main()
     {
 		System.out.println("master checking in massive logasaurus");
-		//heyasdasdasdxfvxcvasds
+		//heyasdasdasdxfvxcvasdswqewwsss
     }
 
     public static String helloWorld()
