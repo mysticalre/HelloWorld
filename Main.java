5946758235ffc3fb714170e8d528cc2878d8bd37
Rakas <drakas@netsuite.com>
2016-09-29 10:34:51 -0400

    empty main

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ public class Main
 {
     public Main()
     {
-		System.out.println("Epic memerz boyz");
-		System.out.println("Epic memerz gurlz");
+
     }
 
     public static String helloWorld()
