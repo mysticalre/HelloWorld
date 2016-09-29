f9bdc6ce0b8d5899a4e806eeb8905cd61148071a
Rakas <drakas@netsuite.com>
2016-09-29 16:51:49 -0400

    ml commit

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
     public Main()
     {
 		System.out.println("master checking in massive logasaurus");
-		//heyasdasdasdxfvxcvassdasddasdsasd
+		//heyasdasdasdxfvassdasddasdsasd
     }
 
     public static String helloWorld()
