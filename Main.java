efd71cddee764499a19eaa2a95eb2af1630dbef6
Rakas <drakas@netsuite.com>
2016-09-29 15:55:00 -0400

    asdasd

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
     public Main()
     {
 		System.out.println("master checking in massive logasaurus");
-		//heyasdasdasdxfvxcvasdswqewwsssasdasdasddasasdasdasdasds
+		//heyasdasdasdxfvxcvasdswqewwsssasdasdasddasasdasdasdasdsasd
     }
 
     public static String helloWorld()
