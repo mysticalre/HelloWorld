1d8bc0cfd6dd36745f0f7b9c590677cd917e16e2
Rakas <drakas@netsuite.com>
2016-09-30 14:03:01 -0400

    b722076c60dcfe0d24ccafa56f10fd4bb553e5a6

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ public class Main
 {
     public Main()
     {
-		System.out.println(helloWorld());
-		//hdfsdfklsdjfkjaaa
+		System.out.println(helloWorld()); //woot!
     }
 
     public static String helloWorld()
     {
-        return "Hello Worlds_03lexDd";
+        return "Hello World";
     }
 }
